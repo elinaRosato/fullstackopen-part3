ef3f2461306f79127cbf8375e4b9a479be968985
Elina Rosato <rosatoelina@gmail.com>
2023-05-10 11:23:36 +0200

    phonebook database step4

--- index.js
+++ index.js
@@ -64,25 +64,24 @@ app.get('/api/persons', (request, response) => {
   })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => {
-    console.log(error)
-    response.status(500).end()
-  })
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end(); // HTTP 404 Not found
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   Person.findByIdAndDelete(id)
-    .then(() => {
+    .then(result => {
       response.status(204).end(); // HTTP 204 No Content
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -112,6 +111,31 @@ app.post('/api/persons', (request, response) => {
     })
   })
 })
+
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+//Error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' }) // HTTP 400 Bad request
+  }
+  next(error)
+}
+
+// Add the error handler middleware
+app.use(errorHandler)
 
 // Start the server
 const PORT = process.env.PORT
