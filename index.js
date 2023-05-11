326ed5e9d7e4734f0e84603875d0ebb157db00a8
Elina Rosato <rosatoelina@gmail.com>
2023-05-11 10:45:45 +0200

    phonebook database step5 and step6

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/persons');
 const app = express();
+require('dotenv').config()
 
 // Define a custom token for logging the request body
 morgan.token('request-body', (request, resesponse) => {
   return JSON.stringify(request.body);
 });
+
+//Error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' }) // HTTP 400 Bad request
+  }
+  next(error)
+}
+
+//Unknown endpoint middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// Add the static middleware
+app.use(cors())
 
 // Add middleware for parsing JSON request bodies
 app.use(express.json());
@@ -15,35 +33,8 @@ app.use(express.json());
 // Add the morgan middleware with custom format
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'));
 
-// Add the static middleware
-app.use(cors())
-
 // Add the cors middleware to show static content
 app.use(express.static('build'))
-
-// Data
-let persons = [
-	{ 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-]
 
 // Routes
 app.get('/', (request, response) => {
@@ -64,7 +55,6 @@ app.get('/api/persons', (request, response) => {
     .then(persons => {
       response.json(persons)
     })
-    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -132,14 +122,8 @@ app.put('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-//Error handler middleware
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' }) // HTTP 400 Bad request
-  }
-  next(error)
-}
+// Add the unknown endpoint middleware
+app.use(unknownEndpoint)
 
 // Add the error handler middleware
 app.use(errorHandler)
