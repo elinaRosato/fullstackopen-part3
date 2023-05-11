c308c8af884ae82f0cdcb763d30ab68569020bd6
Elina Rosato <rosatoelina@gmail.com>
2023-05-11 19:11:44 +0200

    phonebook database step7

--- models/persons.js
+++ models/persons.js
@@ -18,12 +18,12 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3,
+    minlength: 3,
     required: true
   },
   number: {
     type: String,
-    minLength: 3,
+    minlength: 3,
     required: true
   }
 })
